6824d06dfdcdecce54bf1c55b06865b1872cf302
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2024-01-24 21:54:23 +0200

    1: setup Vue.js project and configure with Vue.js router, Pinia state management library, axios package, and TailwindCSS

---
+++ social-media-app/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import "./assets/styles.css"
+import App from './App.vue'
+import { createPinia } from 'pinia';
+import axios from "axios"
+import router from './router'
+
+const pinia = createPinia();
+const app = createApp(App);
+
+axios.defaults.baseURL = "http://localhost:8080"
+
+app.use(pinia)
+app.use(router, axios)
+app.mount('#app')
