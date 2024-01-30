00900aa022fd0086871cd26e318f615503dfbbd1
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2024-01-30 13:02:45 +0200

    20: remove console.logs in user.js store

--- chatter_frontend/src/stores/user.js
+++ chatter_frontend/src/stores/user.js
@@ -25,31 +25,14 @@ export const useUserStore = defineStore({
         this.user.email = localStorage.getItem('user.email');
         this.user.accessToken = localStorage.getItem('user.accessToken');
         this.user.refreshToken = localStorage.getItem('user.refreshToken');
-
-
         await this.handleRefreshToken();
-
-        console.log({
-          status: "initialize user",
-          user: this.user
-        })
       }
     },
 
     handleSetToken(data) {
-      console.log({
-        status: 'BEFORE set token',
-        user:this.user,
-      })
-
       this.user.accessToken = data.access;
       this.user.refreshToken = data.refresh;
       this.user.isAuthenticated = true;
-
-      console.log({
-        status: 'AFTER set token',
-        user: this.user,
-      })
 
       localStorage.setItem('user.accessToken', data.access)
       localStorage.setItem('user.refreshToken', data.refresh)
@@ -58,10 +41,6 @@ export const useUserStore = defineStore({
     },
 
     handleRemoveToken() {
-      console.log({
-        status: 'remove token'
-      })
-
       this.user.id = "";
       this.user.name = "";
       this.user.email = "";
@@ -77,11 +56,6 @@ export const useUserStore = defineStore({
     },
 
     setUserInfo(user) {
-      console.log({
-        status: 'BEFORE set user info',
-        user,
-      })
-
       this.user.id = user.id;
       this.user.name = user.name;
       this.user.email = user.email;
@@ -89,23 +63,15 @@ export const useUserStore = defineStore({
       localStorage.setItem('user.id', user.id);
       localStorage.setItem('user.name', user.name);
       localStorage.setItem('user.email', user.email);
-
-      console.log({
-        status: 'AFTER set user info',
-        user,
-      })
     },
 
     async handleRefreshToken() {
-     console.log({})
      try {
        if (this.user.refreshToken) {
-         console.log("this.user.refreshToken", this.user.refreshToken)
          const response = await axios.post(URLS.refreshToken, {
            refresh: this.user.refreshToken
           })
 
-         console.log("response123", response)
          this.user.accessToken = response.data.access
          localStorage.setItem('user.accessToken', response.data.access)
          axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access}`
