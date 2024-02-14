5545bd6fdd57d029cce4e5d37d6cd1d26e98f3db
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2024-02-14 23:28:30 +0200

    57: refactor SignUpView  page to use the LoaderIcon resuable component

--- chatter_frontend/src/views/SignUpView.vue
+++ chatter_frontend/src/views/SignUpView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="items-center justify-between px-6 py-6 mx-auto border rounded-lg lg:max-w-5xl lg:flex lg:px-12 bg-gray-950 border-lime-300">
+  <div class="items-center justify-between px-6 py-6 mx-auto border rounded-lg lg:max-w-5xl lg:flex lg:px-12 bg-slate-950 border-lime-300">
     <div class="py-4 lg:border-y lg:border-lime-300 lg:w-2/5">
       <h1 class="mb-6 text-xl font-bold text-center text-lime-300">Hello and welcome to Chatter!</h1>
       <p class="text-lg text-center text-slate-200">Sign up today to start chatting with your friends!</p>
@@ -24,12 +24,7 @@
       <div>
         <button class="flex items-center justify-center w-full h-10 px-4 py-2 font-medium rounded-lg bg-lime-300 text-slate-950 disabled:bg-slate-600" :disabled="isLoading">
           <span class="px-4" v-if="!isLoading">Create Account</span>
-          <svg v-if="isLoading" xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
-          	<path fill="currentColor" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity="0.5" />
-          	<path fill="currentColor" d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
-          		<animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
-          	</path>
-          </svg>
+          <LoaderIcon v-if="isLoading" classes="text-slate-950"/>
         </button>
       </div>
       <p class="mt-10 text-sm text-slate-400">Already have an account? Go ahead and <router-link :to="{name:'login'}" class="text-lime-300">Log In</router-link>.</p>
@@ -38,6 +33,8 @@
 </template>
 
 <script>
+import LoaderIcon from '@/components/icons/LoaderIcon.vue';
+
 import URLS from '@/constants/urls';
 import axios from 'axios';
 import { useToast } from "vue-toastification";
@@ -50,7 +47,9 @@ export default {
       toast,
     }
   },
-
+  components: {
+    LoaderIcon
+  },
   data() {
     return {
       form: {
